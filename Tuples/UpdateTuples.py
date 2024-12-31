23dfe50e9b2eddd61b37820379d1f1a6e68b0984
Maya Oniciuc <maya.klmno@gmail.com>
2024-12-31 16:01:25 -0500

    Update Tuples Lesson (start not finished) Dec 31, 2024

---
+++ Tuples/UpdateTuples.py
@@ -0,0 +1,11 @@
+'''
+UPDATE TUPLES:
+- Tupes are unchangeable, meaning that you cannot add, change, or 
+  remove items once the tuple is created. 
+  But there are some workarounds
+
+
+CHANGE TUPLE VALUES:
+- Once a tuple is created, you cannot change its value. 
+  Tuples are unchangeable, or immutable as it is also called.  
+'''
