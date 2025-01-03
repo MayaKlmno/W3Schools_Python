02ea1100a3d5bc0d3e52b969fd059b135a2fd996
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-03 14:11:07 -0500

    Loop Tuples Lesson (not finished) Jan 3, 2025

---
+++ Tuples/LoopTuples.py
@@ -0,0 +1,15 @@
+'''
+LOOP TUPLES: 
+- You can loop through the tuple items by using a for loop
+example: (iterate through the items and print the values)
+'''
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+    print(x)
+
+'''
+LOOP THROUGH THE INDEX NUMBERS:
+- You can also loop through the tuple items by referring to their index number.
+  Use the range() and len() functions to create a suitable iterable. 
+
+'''
