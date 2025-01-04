0a50fbf439c083b89b03da6f2fd5881abb85f61f
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-04 11:35:36 -0500

    Remove Sets Lesson (not finished) Jan 4, 2025

---
+++ Sets/RemoveSetItems.py
@@ -0,0 +1,6 @@
+'''
+REMOVE SET ITEMS:
+- To remove an item in a set, use the remove(), or the discard() method.
+'''
+# example: (remove "banana" by using the remove() method)
+thisset = {"apple", "banana", "cherry"}
