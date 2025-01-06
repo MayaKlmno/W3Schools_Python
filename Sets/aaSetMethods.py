20b3500c7713ccae601e47fa57cc8efcc1937579
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-06 11:42:05 -0500

    Set methods Jan 6, 2025

---
+++ Sets/aaSetMethods.py
@@ -0,0 +1,71 @@
+''' SET METHODS: python has a set of built-in methods that  you can
+                use on sets'''
+
+# add()
+    # adds an element to the set
+    
+# clear()
+    # removes all the elements from the set
+    
+# copy()
+    # returns a copy of the set
+    
+# difference()
+    # shortcut: - 
+    # returns a set containing the difference between two or more sets
+
+# difference_update()
+    # shortcut: -=
+    # removes the items in this set that are also included in another,
+    # specified set
+
+# discard()
+    # removes the specified item
+    
+# intersection()
+    # shortcut: &
+    # returns a set, that is the intersection of two other sets
+    
+# intersection_update()
+    # shortcut: &=
+    # removes the items in this set that are not rpesent in 
+    # other specified set(s)
+    
+# isdisjoint()
+    # returns whether two sets have an intersection or not
+    
+# issubset()
+    # shortcut: <=
+    # returns whether nother set contains this set or not
+    # shortcut: <
+    # returns whether all items in this set is present in other,
+    # specified set(s)
+    
+# issuperset()
+    # shortcut: >=
+    # returns whether this set contains another set or not
+    # shortcut: >
+    # returns whether all tiems in other, specified set(s) is present
+    # in this set
+
+# pop()
+    # removes an element from the set
+    
+# remove()
+    # removes the specified element
+    
+# symmetric_difference()
+    # shortcut: ^
+    # returns a set with the symmetric differences of two sets
+    
+# symmetric_difference_update()
+    # shortcut: ^=
+    # inserts the symmetric differences from this set and another
+    
+# union()
+    # shortcut: |
+    # return a set containing the union of sets
+
+# update()
+    # shortcut: |=
+    # update the set with the union of this set and others
