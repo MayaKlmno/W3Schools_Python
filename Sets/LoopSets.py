54331639bb743538eea37f11d535c4c984c1d759
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-05 19:57:20 -0500

    Looping through sets lesson Jan 5, 2025

---
+++ Sets/LoopSets.py
@@ -0,0 +1,16 @@
+'''
+LOOP ITEMS:
+- You can loop through the set items by using a for loop
+'''
+# example: (loop through the set and print the values)
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+    print(x)
+
+# EXERCISE: What is the correct syntax for looping through the item of a set?
+# 1.) for x in {'apple', 'banana', 'cherry'}:
+#       print(x)        # correct
+# 2.) for x in {'apple', 'banana', 'cherry}
+#       print(x)
+# 3.) foreach x in {'apple', 'banana', 'cherry'}:
+#       print(x)
