7d1d4adbd61db0dba069642e38c5b067061ba17d
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-07 10:51:10 -0500

    Access Dictionary Items Lesson (not finished) Jan 7, 2025

--- Dictionaries/AccessDictionaryItems.py
+++ Dictionaries/AccessDictionaryItems.py
@@ -25,4 +25,72 @@ x = thisdict.keys()
 
 # the list of the keys is a view of the dictionary, meaning that any
     # changes done to the dictionary will be reflected in the keys list
-# example: ()
+# example: (Add a new item to the origional dictionary, and see that the 
+#           keys list gets updated as well)
+car = {
+    "brand" : "Ford",
+    "model" : "mustang",
+    "year" : 1964
+}
+y = car.keys()
+print(y) # this is before the change to the dictonary
+car["color"] = "white" # changing the key for color
+print(y) # now the key set will change as well
+
+'''
+GET VALUES:
+- The values() method will return a list of all the values in the 
+  dictionary
+'''
+# example: (get a list of the values)
+z = thisdict.values()
+
+# the list of the values is a VIEW of the dictionary, meaning that any 
+#   changes done to the dictionary will be reflected in the values 
+#   list
+# example: (make a change in the origional dictionary, and see the
+#           values list gets updated as well)
+car2 = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+a = car2.values()
+print("value of a before: ")
+print(a)
+car2["year"] = 2025
+print("this is a after you change the origional: ")
+print(a)
+    # these are the values NOT the keys
+
+# example: (add a new item to the origional dictionary, and see
+#           that the values list gets updated as well)
+car3 = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+b = car3.values()
+print(b) # this is b beofre the change
+car3["color"] = "red" # we change the value
+print(b) # this is the value of be after the change
+    # now there is a new key and value for the color
+
+'''
+GET ITEMS:
+- The items() method will return each item in a dictionary, as tuples
+  in a list
+'''
+# example: (get a list of the key:value pairs)
+w =thisdict.items()
+print(w)
+    # this will print: dict_items([('brand', 'Ford'), ('model', 'Mustang'), ('year', 1964)])
+
+# the returned list is a view of the items in the dictionary, meaning
+#   that any changes done to the dictionary will be reflected in the items
+#   list.
+# example: (make a change in the origional dictionary, and see that the
+#           items list gets updated as well)
+car4 = {
+    "brand"
+}
