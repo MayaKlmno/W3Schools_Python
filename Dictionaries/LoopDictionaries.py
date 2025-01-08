cf91d4382e8d8beb19382d893770d89bedb93fb4
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-08 18:36:13 -0500

    Looping through dictionaries lesson Jan 8, 2025

---
+++ Dictionaries/LoopDictionaries.py
@@ -0,0 +1,6 @@
+'''
+LOOPING THROUGH A DICTIONARY:
+- You can loop through a dictionary by using a for loop
+- When looping through a dictionary, the return value are the keys
+  of the dictionary, but there are methods to return the values as well
+'''
