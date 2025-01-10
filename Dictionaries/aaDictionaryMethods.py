46cd47d55bbec6681eea3cd4ce103fc17167571a
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-09 20:13:55 -0800

    Different Important methods for Dictionaries Jan 9, 2025

---
+++ Dictionaries/aaDictionaryMethods.py
@@ -0,0 +1,39 @@
+'''
+PYTHON METHODS:
+- python has a set of built-in methods that you can
+  use on dictionaries
+'''
+
+# clear()
+    # removes all the elements from the dictionary
+    
+# copy()
+    # returns a copy of the dictionary
+    
+# fromkeys()
+    # returns a dictionary with the specified keys and value
+    
+# get()
+    # returns the value of the specified key
+    
+# items()
+    # returns a list containing a tuple for each key value pair
+    
+# keys()
+    # returns a list containting the dictionary's keys
+    
+# pop()
+    # returns the element with the specified key
+    
+# popitem()
+    # removes the last inserted key-value pair
+    
+# setdefault()
+    # returns the value of the specified key.
+    # if the key doesn't exist: insert the key, with the specified value
+
+# update()
+    # updates the dictionary with the specified key-value pairs
+    
+# values()
+    # returns a list of all the values in the dictionary
