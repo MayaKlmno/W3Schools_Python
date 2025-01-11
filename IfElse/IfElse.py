f6df5e5341dadc6ef882596d69da139b0e0d1e75
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-10 19:52:25 -0800

    If Else (not finished) Lesson Jan 10, 2025

---
+++ IfElse/IfElse.py
@@ -0,0 +1,91 @@
+'''
+PYTHON CONDITIONS AND IF STATEMENTS:
+- Python supports the usual logical conditions from mathematics:
+    - equals: a == b
+    - not equals: a != b
+    - less than: a < b
+    - less than or equal to: a <= b
+    - greater than: a > b
+    - greater than or equal to: a >= b
+- these conditions can be used in several ways, most commonly in "if statements and loops"
+- an "if statement" is written by using the if keyword
+'''
+# example: (if statement)
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+'''
+- In this example we use two variables, a and b, which are used as part
+  of the if statement to test whether b is greater than a. As a is 33,
+  and b is 200, we know that 200 is greater than 33, and so we print that
+  "b is greater than a"
+'''
+
+'''
+INDENTATION:
+- python relies on indentaiton to define the scope of the code. 
+  other programming languages often use curly-brackets for this 
+  purpose. such as java
+'''
+# example 
+    # if you write an if statement with improper indentaion an 
+    # error will arise
+    
+'''
+ELIF:
+- the elif keyword is python's way of saying:
+  "if the previous conditions were not true, then try this conditon"
+'''
+# example:
+c = 33
+d = 33
+if b > a:
+    print("d is greater than c")
+elif a == b:
+    print("c and d are equal")
+
+# in this example a is equal to b, so the first conditon is
+# not true, but the elif condition is true, so we print to screen
+# that "a and b are equal" 
+
+'''
+ELSE:
+- The else keyword catches anything which isn't cought
+  by the preceding conditions
+'''
+# example:
+e = 200
+f = 33
+if f >  e:
+    print("f is greater than e")
+elif e == f:
+    print("e is equal to f")
+else:
+    print("a is greater than b")
+# in this example e is greater than f, so the first condition is false,
+# the elif conditon is also false, so we have to go to the else condtion
+# and print to screen that "a is greater than b"
+
+# you can also have the else without the elif
+g = 200
+h = 33
+if h > g:
+    print("h is greater than g")
+else:
+    print("h is not greater than g")
+    
+'''
+SHORT HAND IF:
+- if you have only one statement to execte, you can put it on the same line
+  as the if statement
+'''
+# example: (one line if statement)
+if a > b: print("a is greater than b")
+
+'''
+SHORT HAND IF... ELSE:
+- if you have only one statement to execute, on for if, and one for else,
+  you can put it all on the same line
+'''
