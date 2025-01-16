30187bc2552527548e634710cbd1697cf1c06a07
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-15 22:13:17 -0800

    for loops lesson (not finished) jan 15, 2025

--- Loops/ForLoops.py
+++ Loops/ForLoops.py
@@ -82,3 +82,20 @@ for x in range(2,6):
 # example: (increment the sequence with 3 (default is 1))
 for x in range(2, 30, 3):
     print(x)
+    
+    
+'''
+ELSE IN FOR LOOP:
+- The else keyword in a for loop specifies a block of code to be executed
+  when the loop is finished
+'''
+# example: (print all numbers from 0 to 5 and print a message when 
+#           the loop has ended)
+for x in range(6):
+    print(x)
+else:
+    print("finally finished!")
+
+# NOTE: the else block will NOT be executed if the loop is stopped by
+#       a break statement
+# example: ()
