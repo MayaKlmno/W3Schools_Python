9990fc2524d40bb58d5838ea79a63dbd2a99fb75
Maya Oniciuc <maya.klmno@gmail.com>
2025-01-17 22:16:08 -0800

    For Loops lesson not finished Jan 17, 2025

--- Loops/ForLoops.py
+++ Loops/ForLoops.py
@@ -98,4 +98,9 @@ else:
 
 # NOTE: the else block will NOT be executed if the loop is stopped by
 #       a break statement
-# example: ()
+# example: (break the loop when x is 3, and see what happens with the else block)
+for x in range(6):
+    if x == 3: break
+    print(x)
+else:
+    print("finally finished")
