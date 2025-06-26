841467deb3dd2f6e9ecfa849fac98d19a6ca9ec6
Maya Oniciuc <maya.klmno@gmail.com>
2025-06-26 18:35:18 -0400

    06.26.25 Lambda

---
+++ Lambda/PythonLambda.py
@@ -0,0 +1,46 @@
+'''
+PYTHON LAMBDA:
+A lambda function is a small anonymous function.
+A lambda function can take any number of arguments,
+    but can only have one expression.
+''' 
+
+'''
+### Syntax ###
+# lambda arguments : expression
+#  --- the expression is executed and the result is returned
+
+'''
+
+# example: add to 10 to argument a, and return the result:
+x = lambda a : a + 10
+print(x(5))
+
+''' Lambda functions can take any number of arguments: '''
+# example: multiply argument a with argument b and return the result:
+y = lambda a, b : a * b
+print(x(5,6))
+
+# example: summarize argument a, b, and c and return the result:
+z = lambda a, b, c : a + b + c
+print(z(1, 2, 3))
+
+
+'''
+-- WHY USE LAMBDA FUNCTIONS? ---
+The power of lambda is better shown when you use them as an anonymous function
+    inside another function.
+Say you have a function defenition that takes one argument, and that argument
+    will be multiplied with an unknown number:
+'''
+
+def myfunc(n):
+    return lambda a : a * n
+    
+    # use that function defenition to make a function that always
+    # doubles the number you send in:
+def myfunc2(n):
+    return lambda a : a * n
+mydoubler = myfunc(2)
+print(mydoubler(11))
+
